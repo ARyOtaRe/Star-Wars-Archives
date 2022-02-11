f268ca8351626747ece9b931b07b7b30fecf90c6
Raϕ <72984363+ARyOtaRe@users.noreply.github.com>
2022-02-11 22:10:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -49,7 +49,6 @@ class Archives(commands.Bot):
         return await client.process_commands(message)
         
 
-    
 client= Archives(command_prefix=commands.when_mentioned_or('ach!'))
 
 client.remove_command('help')
